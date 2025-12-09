8fab8210c15af1c8877086940ff850b16330fa0d
Emmanuel Egomson <fegomson@gmail.com>
2025-12-09 13:26:46 -0600

    feat: update TicketPage to use async params for ticketId extraction

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,7 +1,8 @@
-const TicketPage = ({ params }) => {
+const TicketPage = async ({ params }: { params: Promise<{ ticketId: string }> }) => {
+    const { ticketId } = await params;
 
     return (
-        <div className="font-bold text-amber-300">Ticket Page {params.ticketId}</div>
+        <div className="font-bold text-amber-300">Ticket Page {ticketId}</div>
     )
 }
 export default TicketPage
