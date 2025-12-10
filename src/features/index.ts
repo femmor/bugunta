d9b2f5da19c7d63c843ee2ebdbde97d988185215
Emmanuel Egomson <fegomson@gmail.com>
2025-12-09 21:54:06 -0600

    feat: restructure components by removing Heading, Navbar, and Placeholder, and updating their imports

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,4 @@
-export { default as Heading } from "./heading/components/Heading";
-export { default as Navbar } from "./navbar/components/Navbar";
-export { default as Placeholder } from "./placeholder/components/Placeholder";
+export { default as Heading } from "./heading";
+export { default as Navbar } from "./navbar";
+export { default as Placeholder } from "./placeholder";
 export { default as Ticket } from "./ticket/components/Ticket";
