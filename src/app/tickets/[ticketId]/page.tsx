5febe5eb9d32eac52b96a164fabcf8bc25c2773e
Emmanuel Egomson <fegomson@gmail.com>
2025-12-09 21:45:46 -0600

    feat: implement Placeholder component for enhanced error handling in TicketPage and update imports

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,5 @@
+import { PiBugDroidDuotone } from "react-icons/pi";
+import { Placeholder } from "@/components";
 import { initialTickets } from "@/data"
 
 type TicketPageProps = {
@@ -8,17 +10,17 @@ const TicketPage = async ({ params }: TicketPageProps) => {
     const { ticketId } = await params;
     const ticket = initialTickets.find(ticket => ticket.id === ticketId);
 
+    if (!ticket) {
+        return <Placeholder label="Ticket not found" icon showButton iconType={<PiBugDroidDuotone className="mx-auto mb-2 text-primary/40" size={60} />} />;
+    }
+
     return (
         <div className="flex flex-col">
-            {ticket ? (
-                <div className="p-4 border">
-                    <h1 className="text-2xl underline mb-2">{ticket.title}</h1>
-                    <p className="mb-2">{ticket.content}</p>
-                    <p>Status: {ticket.status}</p>
-                </div>
-            ) : (
-                <p>Ticket not found.</p>
-            )}
+            {ticket && <div className="p-4 border">
+                <h1 className="text-2xl underline mb-2">{ticket.title}</h1>
+                <p className="mb-2">{ticket.content}</p>
+                <p>Status: {ticket.status}</p>
+            </div>}
         </div>
     )
 }
---
+++ src/components/Placeholder.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import { homePath } from "@/paths";
+import { Button } from "./ui/button";
+
+
+const Placeholder = ({ label, icon, iconType, showButton, buttonLink, buttonText = "Go Home" }: { label: string, icon?: boolean, iconType?: ReactNode, showButton?: boolean, buttonLink?: string, buttonText?: string }) => {
+    return (
+        <div className="flex-1 self-center text-center mt-20 animate-fade-in-from-top">
+            {icon && iconType}
+            <h2 className="text-2xl text-muted-foreground">{label}</h2>
+            {showButton && <Button asChild size="lg" className="bg-gray-500 my-2">
+                <Link href={buttonLink || homePath()}>{buttonText}</Link>
+            </Button>}
+        </div>
+    )
+}
+export default Placeholder
