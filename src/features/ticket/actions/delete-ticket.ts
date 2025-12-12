356ee721ebfc01a279b9e9476eae2c62860eec79
Emmanuel Egomson <fegomson@gmail.com>
2025-12-12 10:17:26 -0600

    feat: update deleteTicket action to redirect after successful deletion

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,13 +1,16 @@
 "use server"
 
+import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/paths";
 
 export const deleteTicket = async (id: string) => {
     try {
         await prisma.ticket.delete({
             where: { id },
         });
-        console.log(`Ticket with id ${id} deleted successfully.`);
+
+        redirect(ticketsPath());
     } catch (error) {
         console.error(`Error deleting ticket with id ${id}:`, error);
         throw error;
