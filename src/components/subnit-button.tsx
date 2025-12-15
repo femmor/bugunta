21b34d48a1bc4bc463ab29b045b6858a87505288
Emmanuel Egomson <fegomson@gmail.com>
2025-12-15 11:01:34 -0600

    feat: implement useFormStatus hook in SubmitButton component for form submission and refactor TicketUpsertForm to use it

---
+++ src/components/subnit-button.tsx
@@ -0,0 +1,18 @@
+import { useFormStatus } from "react-dom"
+import { Button } from "./ui/button"
+import { Spinner } from "./ui/spinner"
+
+type SubmitButtonProps = {
+    label: string
+}
+
+export const SubmitButton = ({ label }: SubmitButtonProps) => {
+    const { pending } = useFormStatus()
+
+    return (
+        <Button type="submit" disabled={pending}>
+            {pending && <Spinner className="mr-2 h-4 w-4" />}
+            {label}
+        </Button>
+    )
+}
--- src/features/ticket/components/TicketUpsertForm.tsx
+++ src/features/ticket/components/TicketUpsertForm.tsx
@@ -1,11 +1,9 @@
 "use client"
 
 import { Ticket } from "@prisma/client"
-import { useTransition } from "react"
-import { Button } from "@/components/ui/button"
+import { SubmitButton } from "@/components/subnit-button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Spinner } from "@/components/ui/spinner"
 import { Textarea } from "@/components/ui/textarea"
 import { upsertTicket } from "../actions/upsert-ticket"
 
@@ -14,26 +12,19 @@ type TicketUpsertFormProps = {
 }
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
-    const [isPending, startTransition] = useTransition()
-
-    const upsertTicketAction = (formData: FormData) => {
-        startTransition(async () => {
-            await upsertTicket.bind(null, ticket?.id)(formData)
-        })
-    }
 
     return (
-        <form className="flex flex-col gap-y-2" action={upsertTicketAction}>
+        <form
+            className="flex flex-col gap-y-2"
+            action={upsertTicket.bind(null, ticket?.id)}
+        >
             <Label htmlFor="title">Title</Label>
             <Input type="text" id="title" name="title" defaultValue={ticket?.title} />
 
             <Label htmlFor="content">Content</Label>
             <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-            <Button type="submit" disabled={isPending}>
-                {isPending && <Spinner className="mr-2 h-4 w-4" />}
-                {ticket ? "Update Ticket" : "Create Ticket"}
-            </Button>
+            <SubmitButton label={ticket ? "Update Ticket" : "Create Ticket"} />
         </form>
     )
 }
