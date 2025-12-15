c1af1f07c7bd1722d7244cb84fbd84b0412c1686
Emmanuel Egomson <fegomson@gmail.com>
2025-12-15 10:40:36 -0600

    feat: enhance TicketUpsertForm with loading state using useTransition hook and refactor upsert action

--- src/features/ticket/components/TicketUpsertForm.tsx
+++ src/features/ticket/components/TicketUpsertForm.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import { Ticket } from "@prisma/client"
+import { useTransition } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Spinner } from "@/components/ui/spinner"
 import { Textarea } from "@/components/ui/textarea"
 import { upsertTicket } from "../actions/upsert-ticket"
 
@@ -10,15 +14,24 @@ type TicketUpsertFormProps = {
 }
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+    const [isPending, startTransition] = useTransition()
+
+    const upsertTicketAction = (formData: FormData) => {
+        startTransition(async () => {
+            await upsertTicket.bind(null, ticket?.id)(formData)
+        })
+    }
+
     return (
-        <form className="flex flex-col gap-y-2" action={upsertTicket.bind(null, ticket?.id)}>
+        <form className="flex flex-col gap-y-2" action={upsertTicketAction}>
             <Label htmlFor="title">Title</Label>
             <Input type="text" id="title" name="title" defaultValue={ticket?.title} />
 
             <Label htmlFor="content">Content</Label>
             <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-            <Button type="submit">
+            <Button type="submit" disabled={isPending}>
+                {isPending && <Spinner className="mr-2 h-4 w-4" />}
                 {ticket ? "Update Ticket" : "Create Ticket"}
             </Button>
         </form>
